6de2f304ceb717bc48d62f81bcfaf007463e942a
huangyaqing <huangyaqing@xinpayroll.com>
2023-05-30 15:30:23 +0800

    server：连接数据库

--- config/config.default.js
+++ config/config.default.js
@@ -23,6 +23,36 @@ module.exports = appInfo => {
     // myAppName: 'egg',
   };
 
+  // 【安全威胁 csrf 的防范】 网络请求的安防策略，配置白名单配置
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: [ '*' ], // 配置白名单
+  };
+
+  // 配置数据库链接
+  exports.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: 'localhost',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '数据库密码',
+      // 数据库名
+      database: 'sunny-tally-book',
+    },
+    // 是否加载到 app 上，默认开启
+    app: true,
+    // 是否加载到 agent 上，默认关闭
+    agent: false,
+  };
+
   return {
     ...config,
     ...userConfig,
