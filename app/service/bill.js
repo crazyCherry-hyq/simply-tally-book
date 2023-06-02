e5b1b6fe233f63de71af3ae7867eac9e953d9bb7
huangyaqing <714099658@qq.com>
2023-06-02 16:22:13 +0800

    fix：去掉console.log

--- app/service/bill.js
+++ app/service/bill.js
@@ -6,7 +6,6 @@ class BillService extends Service {
   async add(params) {
     const { app } = this;
     try {
-      console.log('params', params);
       // 往 bill 表中，插入一条账单数据
       const result = await app.mysql.insert('bill', params);
       return result;
